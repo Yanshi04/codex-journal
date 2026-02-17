3cc87324149329670baaf5f62a09ead65c501899
Yanshi04 <yanahoubrechts04@gmail.com>
2026-02-17 22:47:56 +0100

    Ffinished the beats models and registration.

--- bestiary/admin.py
+++ bestiary/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BeastsGroup, HowToWin, Monster
 
-# Register your models here.
+@admin.register(Monster)
+class MonsterAdmin(admin.ModelAdmin):
+    list_display = ('monster_name', 'level_of_danger', 'kind')
+    list_filter = ('kind',)
+    search_fields = ('monster_name',)
+
+admin.site.register(BeastsGroup)
+admin.site.register(HowToWin)
--- bestiary/models.py
+++ bestiary/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class BeastsGroup(models.Model):
+    group_name = models.CharField(max_length = 60)
+    def __str__(self):
+        return self.group_name
+
+class HowToWin(models.Model):
+    item_name = models.CharField(max_length = 130)
+    def __str__(self):
+        return self.item_name
+
+class Monster(models.Model):
+    monster_name = models.CharField(max_length = 130)
+    my_notes = models.TextField()
+    level_of_danger = models.PositiveIntegerField()
+    image_url = models.URLField()
+
+    kind = models.ForeignKey(BeastsGroup, on_delete = models.CASCADE)
+
+    what_to_use = models.ManyToManyField(HowToWin)
+
+    def __str__(self):
+        return self.monster_name
