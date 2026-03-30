37744bb96ba87ff6e2c4269e506c5363d0538ad6
Yanshi04 <yanahoubrechts04@gmail.com>
2026-03-30 14:35:12 +0200

    Refactored common folder to make sure only CBVs are used.

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import home_page
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,17 +1,19 @@
 # from .models import Profile
 from django.shortcuts import render
 from bestiary.models import Monster
+from django.views.generic import TemplateView
 
-def home_page(request):
-    recent_monsters = Monster.objects.all().order_by('-id')[:3]
-    total_monsters = Monster.objects.count()
-    context = {
-        'recent_monsters': recent_monsters,
-        'total_monsters': total_monsters
 
-    }
-    return render(request, 'common/home.html', context)
+class HomePageView(TemplateView):
+    template_name = 'common/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_monsters'] = Monster.objects.all().order_by('-id')[:3]
+        context['total_monsters'] = Monster.objects.count()
+
+
+        return context
 
 def custom_404(request, exception):
     return render(request, '404.html', status =404)
