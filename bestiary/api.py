4ff5c587b1ef87f45a67ad1086b32d94c5a770bb
Yanshi04 <yanahoubrechts04@gmail.com>
2026-04-01 19:45:54 +0200

    Fix up some issues, including wrong commit from before.

--- bestiary/api.py
+++ bestiary/api.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Monster
 from .serializers import MonsterSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -8,6 +8,10 @@ class MonsterListCreateAPI(generics.ListCreateAPIView):
     serializer_class = MonsterSerializer
     # everyone can read but only logged in users and profiles can edit/create
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['monster_name', 'my_notes']
+    ordering_fields = ['level_of_danger', 'monster_name']
 
     def perform_create(self, serializer):
         serializer.save(hunter = self.request.user.profile)
